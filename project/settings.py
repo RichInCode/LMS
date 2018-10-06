932e33425477ce8eb7d956d803537a4ca52ab49d
RichInCode <richard.petti@gmail.com>
2018-10-06 12:07:33 -0400

    playing with settings.py file

--- project/settings.py
+++ project/settings.py
@@ -126,5 +126,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'frontend/static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
