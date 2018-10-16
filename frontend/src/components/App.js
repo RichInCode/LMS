84e470c52ca6344d71ada75ff450e7467ddcf5e5
RichInCode <richard.petti@gmail.com>
2018-10-16 10:47:20 -0400

    adding architecture for chatbot

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,12 +7,27 @@ import Form from "./Form";
 import Landing from './LandingPage';
 import NavBar from './NavBar';
 import '../../static/css/App.css';
+import MessageList from "./ChatBot";
 
 import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
+
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
 
 class App extends Component {
   constructor(props) {
     super(props);
+    tihs.state = {
+      messages: DUMMY_DATA
+      }
     }
   render() {
     return (
@@ -41,6 +56,19 @@ class App extends Component {
                   render={data => <Table data={data} />} />
             )}
           />
+          <Route
+            exact
+            path="/lead/chatbot"
+            render={props => (
+              <Title />
+              <MessageList messages={this.state.messages} />
+              <SendMessageForm />
+            )}
+          />
+        </div>
+     </div>
+    )
+  }
         </div>
       </div>
     );
